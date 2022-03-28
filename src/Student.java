295a3ea118937fe8b3ed5378100427ee16d5749e
AlexandrMikyanets <mikyanecoleg@mail.ru>
2022-03-28 19:41:23 +0300

    asd name

--- src/Student.java
+++ src/Student.java
@@ -2,31 +2,15 @@ public class Student {
     private  String name;
     private  int age;
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public  void  asd(String name){
-        System.out.println(getName());
-
-    }
     public void asd(int age){
-        System.out.println(getAge());
+        System.out.println(age);
 
     }
     public void asd(String name,int age){
-        System.out.println( getName() + getAge());
+        System.out.println( name +age);
+    }
+    public void asd(String name){
+        System.out.println(name);
     }
 }
