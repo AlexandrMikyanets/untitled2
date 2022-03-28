eb076aba52148422e851e75cfb871be28c46ccf6
AlexandrMikyanets <mikyanecoleg@mail.ru>
2022-03-28 19:44:47 +0300

    asd age

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Student student = new Student();
         student.asd("alex");
-        student.asd(30);
+        student.asd(29);
 
     }
 }
